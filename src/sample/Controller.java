74176d09e56b6c01a0e2a57cfe32ac4c040bc124
Marius <mariuspetric52@gmail.com>
2021-03-10 18:29:19 +0100

    a

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,74 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+
 public class Controller {
+
+    @FXML
+    AnchorPane charizard;
+    @FXML
+    AnchorPane mew;
+    @FXML
+    AnchorPane ninetiles;
+    @FXML
+    AnchorPane articuno;
+    @FXML
+    AnchorPane mewtwo;
+    @FXML
+    AnchorPane snorlax;
+
+
+
+    public void click1(MouseEvent mouseEvent) {
+        charizard.setStyle("-fx-background-color: #00FFD1;");
+        mew.setStyle("-fx-background-color: #06799B;");
+        ninetiles.setStyle("-fx-background-color: #06799B;");
+        articuno.setStyle("-fx-background-color: #06799B;");
+        mewtwo.setStyle("-fx-background-color: #06799B;");
+        snorlax.setStyle("-fx-background-color: #06799B;");
+    }
+    public void click2(MouseEvent mouseEvent) {
+        charizard.setStyle("-fx-background-color: #06799B;");
+        mew.setStyle("-fx-background-color: #00FFD1;");
+        ninetiles.setStyle("-fx-background-color: #06799B;");
+        articuno.setStyle("-fx-background-color: #06799B;");
+        mewtwo.setStyle("-fx-background-color: #06799B;");
+        snorlax.setStyle("-fx-background-color: #06799B;");
+    }
+    public void click3(MouseEvent mouseEvent) {
+        charizard.setStyle("-fx-background-color: #06799B;");
+        mew.setStyle("-fx-background-color: #06799B;");
+        ninetiles.setStyle("-fx-background-color: #00FFD1;");
+        articuno.setStyle("-fx-background-color: #06799B;");
+        mewtwo.setStyle("-fx-background-color: #06799B;");
+        snorlax.setStyle("-fx-background-color: #06799B;");
+    }
+    public void click4(MouseEvent mouseEvent) {
+        charizard.setStyle("-fx-background-color: #06799B;");
+        mew.setStyle("-fx-background-color: #06799B;");
+        ninetiles.setStyle("-fx-background-color: #06799B;");
+        articuno.setStyle("-fx-background-color: #00FFD1;");
+        mewtwo.setStyle("-fx-background-color: #06799B;");
+        snorlax.setStyle("-fx-background-color: #06799B;");
+    }
+    public void click5(MouseEvent mouseEvent) {
+        charizard.setStyle("-fx-background-color: #06799B;");
+        mew.setStyle("-fx-background-color: #06799B;");
+        ninetiles.setStyle("-fx-background-color: #06799B;");
+        articuno.setStyle("-fx-background-color: #06799B;");
+        mewtwo.setStyle("-fx-background-color: #00FFD1;");
+        snorlax.setStyle("-fx-background-color: #06799B;");
+    }
+    public void click6(MouseEvent mouseEvent) {
+        charizard.setStyle("-fx-background-color: #06799B;");
+        mew.setStyle("-fx-background-color: #06799B;");
+        ninetiles.setStyle("-fx-background-color: #06799B;");
+        articuno.setStyle("-fx-background-color: #06799B;");
+        mewtwo.setStyle("-fx-background-color: #06799B;");
+        snorlax.setStyle("-fx-background-color: #00FFD1;");
+    }
 }
+
